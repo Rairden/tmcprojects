f255e55af26326eb2a691f8c9d3733e6f0834459
Rairden <erik.rairden@gmail.com>
2018-12-15 07:45:41 -0600

    completed 92

--- mooc-2013-OOProgrammingWithJava-PART1/week5-092.DifferenceDate/src/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week5-092.DifferenceDate/src/Main.java
@@ -2,6 +2,43 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write testcode here
+
+        MyDate first    = new MyDate(24, 12, 2009);
+        MyDate second   = new MyDate(1, 1, 2011);
+        MyDate third    = new MyDate(25, 12, 2010);
+
+        System.out.println(first + " and " + second + " difference in years: " + second.differenceInYears(first) );
+        System.out.println(second + " and " + first + " difference in years: " + first.differenceInYears(second) );
+        System.out.println(first + " and " + third + " difference in years: " + third.differenceInYears(first) );
+        System.out.println(third + " and " + first + " difference in years: " + first.differenceInYears(third) );
+        System.out.println(third + " and " + second + " difference in years: " + second.differenceInYears(third) );
+        System.out.println(second + " and " + third + " difference in years: " + third.differenceInYears(second) );
+
+/*
+        24.12.2009  and 1.1.2011    difference in years: 1
+        1.1.2011    and 24.12.2009  difference in years: 1
+        24.12.2009  and 25.12.2010  difference in years: 1
+        25.12.2010  and 24.12.2009  difference in years: 1
+        1.1.2011    and 25.12.2010  difference in years: 0
+        25.12.2010  and 1.1.2011    difference in years: 0
+*/
+
+        /*
+        ## HH
+        12/28/2001
+        11/27/2000
+
+        ## HL
+        12/2/2001
+        11/28/2000
+
+        ## LH
+        11/28/2001
+        12/2/2000
+
+        ## LL
+        11/27/2001
+        12/28/2000
+        */
     }
 }
--- mooc-2013-OOProgrammingWithJava-PART1/week5-092.DifferenceDate/src/MyDate.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week5-092.DifferenceDate/src/MyDate.java
@@ -1,33 +1,65 @@
+
 public class MyDate {
+
     private int day;
     private int month;
     private int year;
 
-    public MyDate(int day, int montd, int year) {
-        this.day = day;
-        this.month = montd;
-        this.year = year;
+    public MyDate(int day, int month, int year) {
+        this.day    = day;
+        this.month  = month;
+        this.year   = year;
     }
 
-    public String toString() {
-        return this.day + "." + this.month + "." + this.year;
+    public int differenceInYears(MyDate compareDate) {
+        // If years are equal, then you cannot have 1 full year.  Missing one day.
+        if (this.year == compareDate.year) { return 0; }
+
+        // If this year > other year
+        if (!this.earlier(compareDate)) {
+                // HH   (high-high, high-low, etc for MM/DD)
+            if (this.month >= compareDate.month && this.day > compareDate.day
+            || (this.month == compareDate.month && this.day == compareDate.day)) {
+                return this.year - compareDate.year;
+            }   // HL
+            if (this.month >= compareDate.month && this.day < compareDate.day) {
+                return (this.year - compareDate.year) - 1;
+            }   // LH
+            if (this.month < compareDate.month && this.day > compareDate.day) {
+                return (this.year - compareDate.year) - 1;
+            }
+        }
+        // If this year < other year
+        if (this.earlier(compareDate)) {
+                // HH   (high-high, high-low, etc for MM/DD)
+            if (compareDate.month >= this.month && compareDate.day > this.day
+            || (compareDate.month == this.month && compareDate.day == this.day)) {
+                return compareDate.year - this.year;
+            }   // HL
+            if (compareDate.month >= this.month && compareDate.day < this.day) {
+                return (compareDate.year - this.year) - 1;
+            }   // LH
+            if (compareDate.month < this.month && compareDate.day > this.day) {
+                return (compareDate.year - this.year) - 1;
+            }
+        }
+        return 0;
     }
 
     public boolean earlier(MyDate compared) {
         if (this.year < compared.year) {
             return true;
         }
-
         if (this.year == compared.year && this.month < compared.month) {
             return true;
         }
-
-        if (this.year == compared.year && this.month == compared.month
-                && this.day < compared.day) {
+        if (this.year == compared.year && this.month == compared.month && this.day < compared.day) {
             return true;
         }
-
         return false;
     }
 
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
 }
