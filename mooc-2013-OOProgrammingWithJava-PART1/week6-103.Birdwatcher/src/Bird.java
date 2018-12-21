ad70d8e0d6d9d640b2dfb7b5680523aaa317444b
Rairden <erik.rairden@gmail.com>
2018-12-21 09:21:13 -0600

    fix

---
+++ mooc-2013-OOProgrammingWithJava-PART1/week6-103.Birdwatcher/src/Bird.java
@@ -0,0 +1,35 @@
+
+public class Bird {
+
+    private String bird;
+    private String latinBird;
+    private int birdCnt;
+
+    public Bird(String bird) {
+        this.bird = bird;
+    }
+
+    public Bird(String bird, String latinBird) {
+        this.bird      = bird;
+        this.latinBird = latinBird;
+    }
+
+    public void incrementBird(Bird bird) {
+        bird.birdCnt++;
+    }
+
+    public String birdName() {
+        return this.bird;
+    }
+
+    public String birdLatinName() {
+        return this.latinBird;
+    }
+
+    public String toString() {
+        // Seagull (Dorkus Dorkus): 2 observations
+        return bird + " (" + latinBird + "): " + birdCnt + " observations";
+    }
+
+
+}
---
+++ mooc-2013-OOProgrammingWithJava-PART1/week6-103.Birdwatcher/src/Main.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String key = "";
+
+        BirdDB db = new BirdDB();
+
+        while (key != "Quit") {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("?\n");
+            key = scan.nextLine();
+
+            switch (key) {
+                case "Add":
+                    db.add(scan);
+                    break;
+                case "Observation":
+                    db.observation(scan);
+                    break;
+                case "Statistics":
+                    db.statistics();
+                    break;
+                case "Show":
+                    db.show(scan);
+                    break;
+                case "Quit":
+                    key = "Quit";
+                    break;
+            }
+
+        }
+
+
+    }
+}
