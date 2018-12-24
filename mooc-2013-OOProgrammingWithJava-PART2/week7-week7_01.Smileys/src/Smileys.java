b0b4a9d7d574472696d56fa46fd0b03d0033f9c4
erik <erik.rairden@gmail.com>
2018-12-23 20:44:44 -0600

    completed 01

--- mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week7-week7_01.Smileys/src/Smileys.java
@@ -3,9 +3,33 @@ public class Smileys {
 
     public static void main(String[] args) {
         // Test your method at least with the following
-        // printWithSmileys("Method");
-        // printWithSmileys("Beerbottle");
-        // printWithSmileys("Interface");
+         printWithSmileys("Method");
+         printWithSmileys("Beerbottle");
+         printWithSmileys("Interface");
+
+    }
+
+    private static void printWithSmileys(String characterString) {
+        String smiley = ":)";
+
+        if (characterString.length() % 2 == 1) {
+            characterString += " ";
+        }
+
+        String output = smiley + " " + characterString + " " + smiley;
+
+        for (int i = 0; i < output.length()/2; i++) {
+            System.out.print(smiley);
+        }
+
+        System.out.println();
+        System.out.print(output);
+        System.out.println();
+
+        for (int i = 0; i < output.length()/2; i++) {
+            System.out.print(smiley);
+        }
+        System.out.println();
     }
 
 }
