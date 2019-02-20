ed38a772d8ca6e581df815ca6cb373bfe909c18e
Rairden <erik.rairden@gmail.com>
2019-02-20 12:48:57 -0600

    comment

--- mooc-2013-OOProgrammingWithJava-PART2/week8-week8_16.SkiJumping/src/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week8-week8_16.SkiJumping/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        // this is a comments
         Scanner scan = new Scanner(System.in);
         UserInterface ui = new UserInterface(scan);
 
