09530300061dc29860bdbaa665c94eb8a5be7fb7
erik <erik.rairden@gmail.com>
2019-03-30 02:08:26 -0500

    init 25

---
+++ mooc-2013-OOProgrammingWithJava-PART2/week9-week9_25.DuplicateRemover/src/tools/PersonalDuplicateRemover.java
@@ -0,0 +1,26 @@
+package tools;
+
+import java.util.Set;
+
+public class PersonalDuplicateRemover implements DuplicateRemover {
+
+    @Override
+    public void add(String characterString) {
+
+    }
+
+    @Override
+    public int getNumberOfDetectedDuplicates() {
+        return 0;
+    }
+
+    @Override
+    public Set<String> getUniqueCharacterStrings() {
+        return null;
+    }
+
+    @Override
+    public void empty() {
+
+    }
+}
