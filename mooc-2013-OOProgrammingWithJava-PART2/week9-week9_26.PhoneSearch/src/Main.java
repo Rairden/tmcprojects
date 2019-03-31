bb38aefc1ad0f444d07c15fa36105bd7b03674d7
erik <erik.rairden@gmail.com>
2019-03-31 01:53:42 -0500

    init 26

--- mooc-2013-OOProgrammingWithJava-PART2/week9-week9_26.PhoneSearch/src/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week9-week9_26.PhoneSearch/src/Main.java
@@ -2,7 +2,11 @@
 public class Main {
 
     public static void main(String[] args) {
+
         // ATTENTION: In your program, you can create only one instance of class Scanner!
+
+        UserInterface ui = new UserInterface();
+
 
     }
 }
