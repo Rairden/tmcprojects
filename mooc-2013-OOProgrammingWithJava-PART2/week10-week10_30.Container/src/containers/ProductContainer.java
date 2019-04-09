31d7cadc1f5b15c25a8da524271a9a9b9b278092
erik <erik.rairden@gmail.com>
2019-04-09 00:09:58 -0500

    30.2

---
+++ mooc-2013-OOProgrammingWithJava-PART2/week10-week10_30.Container/src/containers/ProductContainer.java
@@ -0,0 +1,28 @@
+package containers;
+
+public class ProductContainer extends Container {
+
+    private String productName;
+
+    public ProductContainer(double capacity) {
+        super(capacity);
+    }
+
+    public ProductContainer(String productName, double capacity) {
+        super(capacity);
+        this.productName = productName;
+    }
+
+    public void setName(String newName) {
+        this.productName = newName;
+    }
+
+    public String getName() {
+        return productName;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
